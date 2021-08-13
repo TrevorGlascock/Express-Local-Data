94093d99a03fc00a79b65b8cf2f24cfc386244a9
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-08-13 17:32:37 -0500

    removed console.error statement for easier debugging

--- src/errors/errorHandler.js
+++ src/errors/errorHandler.js
@@ -1,5 +1,4 @@
 module.exports = function errorHandler(error, req, res, next) {
   const { status = 500, message = "Internal Server Error" } = error;
-  console.error(error);
   res.status(status).json({ error: message });
 };
