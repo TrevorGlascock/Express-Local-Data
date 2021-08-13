72b252bfd68254cd18ddd4152d0df6a4a855ca3d
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-08-13 16:39:47 -0500

    Wrote middleware and LCRUD operations for urls.controller

---
+++ src/urls/urls.controller.js
@@ -0,0 +1,70 @@
+const urls = require("../data/urls-data");
+
+/******************************* Middleware *******************************/
+function bodyHasHref(req, res, next) {
+  // grab the href from the request body
+  const { data: { href } = {} } = req.body;
+  // if href exists, set it to a local variable and proceed to next middleware
+  if (href) {
+    res.locals.href = href;
+    return next();
+  }
+  // otherwise throw a 400 error
+  next({ status: 400, message: "An 'href' property is required." });
+}
+
+function urlExists(req, res, next) {
+  // grab the urlId from the path params
+  const urlId = Number(req.params.urlId);
+  // find a matching url obj in the urls global array
+  const foundUrl = urls.find((url) => url.id === urlId);
+
+  // if such a url exists, set it to a local variable and proceed to the next middleware
+  if (foundUrl) {
+    res.locals.foundUrl = foundUrl;
+    return next();
+  }
+
+  // otherwise throw a 404 error
+  next({
+    status: 404,
+    message: `Url id not found: ${req.params.urlId}`,
+  });
+}
+
+/******************************* L-CRUD *******************************/
+
+function list(req, res) {
+  res.json({ data: urls });
+}
+
+function create(req, res) {
+  // create a new url obj with unique id and the href from the body
+  const newUrl = {
+    id: urls.length + 1,
+    href: res.locals.href,
+  };
+  // push the new url obj into the global urls array and return the obj as JSON
+  urls.push(newUrl);
+  res.status(201).json({ data: newUrl });
+}
+
+function read(req, res) {
+  // return the found url object as JSON
+  res.json({ data: res.locals.foundUrl });
+}
+
+function update(req, res) {
+  // Set found url's href equal to the href from the body
+  res.locals.foundUrl.href = res.locals.href;
+  // return the found url object as JSON
+  res.json({ data: res.locals.foundUrl });
+}
+
+module.exports = {
+  create: [bodyHasHref, create],
+  list,
+  read: [urlExists, read],
+  update: [urlExists, bodyHasHref, update],
+  urlExists: urlExists,
+};
